70ad3614217957985cbc2570a654019b817a9b17
solaredgepros <davidm@solaredgepros.com>
2024-04-15 16:35:18 -0500

    Completed the reveal-on-scroll behavior

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,11 +1,17 @@
 import '../styles/styles.css'
-import MobileMenu from './modules/MobileMenu'
+import MobileMenu from './modules/MobileMenu.js'
+import RevealonScroll from './modules/RevealonScroll.js'
+
+new RevealonScroll(document.querySelectorAll(".feature-item"), 75);
+new RevealonScroll(document.querySelectorAll(".testimonial"), 60);
 
 let mobileMenu = new MobileMenu();
 
 if (module.hot) {
     module.hot.accept()
 }
+
+
 
 
 
