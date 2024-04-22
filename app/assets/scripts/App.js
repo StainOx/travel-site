e09f8f7519a08816549dcd748a21658b4097f817
solaredgepros <davidm@solaredgepros.com>
2024-04-22 16:30:45 -0500

    Completed sticky header functionality.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,9 @@
 import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu.js'
 import RevealonScroll from './modules/RevealonScroll.js'
+import StickyHeader from './modules/stickyHeader.js'
 
+let stickyHeader = new StickyHeader()
 new RevealonScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealonScroll(document.querySelectorAll(".testimonial"), 60);
 
