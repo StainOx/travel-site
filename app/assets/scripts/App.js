386ec7144ca720a612f6ffe701728ec1860a6e83
solaredgepros <davidm@solaredgepros.com>
2024-04-24 16:27:42 -0500

    Compeleted the modal overlay.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,11 +3,25 @@ import MobileMenu from './modules/MobileMenu.js'
 import RevealonScroll from './modules/RevealonScroll.js'
 import StickyHeader from './modules/stickyHeader.js'
 
-let stickyHeader = new StickyHeader()
+new StickyHeader()
 new RevealonScroll(document.querySelectorAll(".feature-item"), 75);
 new RevealonScroll(document.querySelectorAll(".testimonial"), 60);
+let modal
+new MobileMenu();
 
-let mobileMenu = new MobileMenu();
+document.querySelectorAll(".open-modal").forEach(el => {
+    el.addEventListener("click", e => {
+        e.preventDefault()
+        if (typeof modal == "undefined") {
+            import(/* webpackChunkName: "modal" */'./modules/Modal').then(x => {
+                modal = new x.default()
+                setTimeout(() => modal.openTheModal(), 20)
+            }).catch(() => console.log("There was a problem."))
+        } else {
+            modal.openTheModal()
+        }
+    })
+})
 
 if (module.hot) {
     module.hot.accept()
