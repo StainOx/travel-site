fa386fa327adc4ab0de3dc9246bd80911cc4c767
solaredgepros <davidm@solaredgepros.com>
2024-04-29 14:07:33 -0500

    Completed our build process section.

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,32 @@
+import '../styles/styles.css'
+import MobileMenu from './modules/MobileMenu.js'
+import RevealonScroll from './modules/RevealonScroll.js'
+import StickyHeader from './modules/stickyHeader.js'
+new StickyHeader()
+new RevealonScroll(document.querySelectorAll(".feature-item"), 75);
+new RevealonScroll(document.querySelectorAll(".testimonial"), 60);
+let modal
+new MobileMenu();
+AudioListener("checkone")
+document.querySelectorAll(".open-modal").forEach(el => {
+    el.addEventListener("click", e => {
+        e.preventDefault()
+        if (typeof modal == "undefined") {
+            import(/* webpackChunkName: "modal" */'./modules/Modal').then(x => {
+                modal = new x.default()
+                setTimeout(() => modal.openTheModal(), 20)
+            }).catch(() => console.log("There was a problem."))
+        } else {
+            modal.openTheModal()
+        }
+    })
+})
+
+if (module.hot) {
+    module.hot.accept()
+}
+
+
+
+
+
